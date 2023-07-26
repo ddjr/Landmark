2bb9931bee076f52f74ac4824def36328a6de25f
David Daly <ddalyjr@gmail.com>
2023-07-26 10:33:19 -0400

    Initial Commit

---
+++ Landmark/LandmarkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkApp.swift
+//  Landmark
+//
+//  Created by David Daly on 7/26/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
